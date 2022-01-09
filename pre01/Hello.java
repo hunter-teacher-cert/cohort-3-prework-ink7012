7295e4cb0521bb1f00a0350b9bcae8ea238ce60f
ink7012 <97136401+ink7012@users.noreply.github.com>
2022-01-09 00:19:35 +0000

    Copied and pasted code from GitHub pre-work... still doesn't print. -- No errors show up however.

--- pre01/Hello.java
+++ pre01/Hello.java
@@ -1,12 +1,9 @@
 import java.io.*;
 import java.util.*;
 
-public class Hello{
-
-  public static void main(String[] args){
-
-    System.out.println("Hello, World!\nHow are you?");
-
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
 }
-
-}
+    
